c35a1f640d52ca0089d0778a47044984e5a6c263
ivanb <ivanb@DESKTOP-38U1I4V>
2023-04-04 23:24:55 +0300

    Добавьте файлы проекта.

---
+++ project_pp/Model/DataBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_pp.Model
+{
+    internal class DataBase
+    {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ivanb\source\repos\PP_project_bybko\PP_project_bybko\Database1.mdf;Integrated Security=True");
+
+        public void openConnecion()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Closed)
+            {
+                sqlConnection.Open();
+            }
+        }
+        public void closeConnecion()
+        {
+            if (sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public SqlConnection getConnection()
+        {
+            return sqlConnection;
+        }
+    }
+}
---
+++ project_pp/Model/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project_pp.Model
+{
+    internal class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string description { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+        public Decimal budget { get; set; }
+    }
+}
